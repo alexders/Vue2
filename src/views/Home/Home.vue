b79bce65f343372381a868ab9e6eae450b26cb7f
td_code <1098691502@qq.com>
2021-10-28 22:22:15 +0800

    卡片样式

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,22 +1,100 @@
 <template>
-  <div>Home</div>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userImg" alt="" />
+          <div class="userinfo">
+            <p class="name">Admin</p>
+            <p class="access">管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2021/10/28</span></p>
+          <p>上次登录地点：<span>北京</span></p>
+        </div>
+      </el-card>
+      <el-card shadow="hover" style="height: 522px; margin-top: 20px">
+        鼠标悬浮时显示
+      </el-card>
+    </el-col>
+    <el-col :span="16">
+      <div class="num">
+        <el-card shadow="hover" v-for="item in countData" :key="item.name">
+          <i class="icon" :class="`el-icon-${item.icon}`" :style="{background:item.color}"></i>
+          <div class="detail">
+            <p class="num"> ${{item.value}}</p>
+            <p class="txt">{{item.name}}</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card shadow="hover"> <div style="height: 280px"></div> </el-card>
+      <div class="graph">
+        <el-card shadow="hover"> <div style="height: 260px"></div> </el-card>
+        <el-card shadow="hover"> <div style="height: 260px"></div></el-card>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      userImg: require("../../assets/img/avator.jpg"), //使用require 将图片当成模块引入，否则会被当成字符串
+      countData: [
+        {
+          name: "今日支付订单",
+          value: "999",
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "今日收藏订单",
+          value: "90",
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "今日未支付订单",
+          value: "4",
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+        {
+          name: "本月支付订单",
+          value: "9949",
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "本月收藏订单",
+          value: "955",
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "本月未支付订单",
+          value: "91",
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+      ],
+    };
+  },
   mounted() {
-    this.$http.get('/home/getData').then(
-      res=>{
+    this.$http.get("/home/getData").then(
+      (res) => {
         console.log(res.data);
       },
-      err=>{
-        console.log(err)
+      (err) => {
+        console.log(err);
       }
-    )
+    );
   },
-}
+};
 </script>
 
-<style>
-
+<style lang='scss' sccoped>
+@import "~@/assets/scss/home.scss";
 </style>
