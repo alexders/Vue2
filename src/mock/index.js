8c527a1a0a7a9ad83ca831cd777faf40aed45501
td_code <1098691502@qq.com>
2021-11-04 00:18:47 +0800

    用户管理组件创建和mock数据的添加

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,6 @@
 import Mock from 'mockjs'
 import httpApi from './home'
+import userApi from './user'
 //模拟获取数据时间 20-200毫秒延时
 Mock.setup({
     timeout:'20-200'
@@ -7,3 +8,11 @@ Mock.setup({
 
 // 拦截home路径的ajax请求
 Mock.mock(/\/home\/getData/,'get',httpApi.getHmoeDate);
+
+// 用户页相关
+Mock.mock(/\/user\/getUser/, 'get', userApi.getUserList);
+Mock.mock(/\/user\/del/, 'get', userApi.deleteUser);
+Mock.mock(/\/user\/batchremove/, 'get', userApi.batchremove);
+Mock.mock(/\/user\/add/, 'post', userApi.createUser);
+Mock.mock(/\/user\/edit/, 'post', userApi.updateUser);
+//Mock.mock(/\/home\/getData/, 'get', httpApi.getHomeData());
--- src/views/UserManger/UserManger.vue
+++ src/views/UserManger/UserManger.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>Userpage</div>
+  <div class="manage">
+    <div class="manger-header"><common-form></common-form></div>
+    <div class="manger-content"><common-table></common-table></div>
+  </div>
 </template>
 
 <script>
+import CommonForm from '../../components/CommonForm.vue'
+import CommonTable from '../../components/CommonTable.vue'
 export default {
+  components: { CommonForm, CommonTable },
 
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import '~@/assets/scss/common';
 </style>
