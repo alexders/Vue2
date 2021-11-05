243d80825ca485e735382cc63bd3772f672a0730
td_code <1098691502@qq.com>
2021-11-05 23:34:53 +0800

    用户搜索头部样式完成

--- src/components/CommonForm.vue
+++ src/components/CommonForm.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>form</div>
+
+<el-form inline :model="form" ref="form" label-with="100px">
+  <el-form-item v-for="item in formLable" :key="item.model" >
+    <!--form[item.model]类似于  -->
+
+  <el-input  v-model="form[item.model]" :placeholder="'请输入'+item.label" v-if=" !item.type "></el-input>
+
+   <el-select v-model="form[item.model]" placeholder="请选择"  v-if="item.type === 'select'">
+
+      <el-option v-for="item in item.options" :key="item.value" :label="item.label"></el-option>
+
+  </el-select>
+
+    <el-switch v-model="form[item.model]" v-if="item.type === 'switch'"></el-switch>
+</el-form-item>
+<!-- 按钮插槽 -->
+  <el-form-item >
+    <slot></slot>
+  </el-form-item>
+</el-form>
 </template>
 
 <script>
 export default {
-
+  props: {
+    inline:Boolean,  //默认为false
+    form: Object,
+    formLable:Array,
+  },
+  data() {
+    return {
+    }
+  },
 }
 </script>
 
--- src/views/UserManger/UserManger.vue
+++ src/views/UserManger/UserManger.vue
@@ -1,19 +1,41 @@
 <template>
   <div class="manage">
-    <div class="manger-header"><common-form></common-form></div>
+    <div class="manage-header">
+      <el-button type="primary" class="add">+ 新增</el-button>
+      <common-form
+        inline
+        :formLable="formLable"
+        :form="searchForm"
+      >
+      <el-button type="primary">搜索</el-button>
+      </common-form>
+    </div>
     <div class="manger-content"><common-table></common-table></div>
   </div>
 </template>
 
 <script>
-import CommonForm from '../../components/CommonForm.vue'
-import CommonTable from '../../components/CommonTable.vue'
+import CommonForm from "../../components/CommonForm.vue";
+import CommonTable from "../../components/CommonTable.vue";
 export default {
   components: { CommonForm, CommonTable },
-
-}
+  data() {
+    return {
+      searchForm: {
+        keyWords: "",
+      },
+      formLable: [
+        {
+          model: "keyWords",
+          label: "",
+          options:[]
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-@import '~@/assets/scss/common';
+@import "~@/assets/scss/common";
 </style>
